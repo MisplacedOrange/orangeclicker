63194072c75fba0aace783ddb50e156dff1526e1
Roy Lu <Roylu0404@gmail.com>
2026-04-27 09:49:11 -0400

    feat: Features skeleton

--- orangeclicker/src-tauri/src/lib.rs
+++ orangeclicker/src-tauri/src/lib.rs
@@ -1,14 +1,43 @@
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+fn start_clicking() {
+    // TODO: start click loop/task
+}
+
+#[tauri::command]
+fn stop_clicking() {
+    // TODO: stop click loop/task
+}
+
+#[tauri::command]
+fn set_clicks_per_second(cps: u32) {
+    // TODO: update click rate while running or before start
+    let _ = cps;
+}
+
+#[tauri::command]
+fn foreground_clicks(enabled: bool) {
+    // TODO: enforce foreground-only clicks
+    let _ = enabled;
+}
+
+#[tauri::command]
+fn taskbar_clicks(enabled: bool) {
+    // TODO: allow/disallow clicking in taskbar
+    let _ = enabled;
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![
+            start_clicking,
+            stop_clicking,
+            set_clicks_per_second,
+            target_foreground,
+            taskbar_clicks
+        ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
